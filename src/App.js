d701a2e8e74452a78308fcef5d42132fe78b8316
Masih-Saldanha <masih.saldanha@gmail.com>
2022-05-19 13:21:56 -0300

    Função de enviar dados feita

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function App() {
+    const [produto, setProduto] = useState("");
+    const [quantidade, setQuantidade] = useState(0);
+
+    function enviarProduto(e) {
+        e.preventDefault();
+        const url = "http://localhost:5000/produtos";
+        const promise = axios.post(url, { Produto: produto, Quantidade: quantidade });
+        promise.then(response => {
+            alert(response.data);
+        });
+        promise.catch(error => {
+            alert(error.response.data);
+        });
+    }
     return (
-        <h1>TESTE</h1>
+        <form onSubmit={enviarProduto}>
+            <input
+                type="text"
+                placeholder="Nome do Produto"
+                value={produto}
+                onChange={(e) => setProduto(e.target.value)}
+            />
+            <input
+                type="number"
+                placeholder="Quantidade"
+                value={quantidade}
+                onChange={(e) => setQuantidade(e.target.value)}
+            />
+            <button type="submit">Enviar</button>
+        </form>
     )
 }
